940bea2b9b2ed98d5eb6cd3ea4e0d25d81588c83
Chen Liu <leo168haha@gmail.com>
2016-01-23 21:09:36 -0500

    Step 2 done

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ app.controller('MainController', ['$scope', function ($scope) {
     $scope.rOutput = '';
     /* Array of arrays, each 2nd level element is a cell in the table */
     $scope.parsed = [];
+    /* Current column hovered on and that should be highlighted */
     $scope.columnHoveredOn = -1;
+    /* Array of booleans: for column i, true == selected, false == not selected */
+    $scope.selectedColumns = [];
     
     $scope.loadExample = function () {
         $scope.rOutput = '(Intercept)                  3.059e+05  1.096e+04  27.919  < 2e-16 ***\n' +
@@ -20,9 +23,12 @@ app.controller('MainController', ['$scope', function ($scope) {
             return;
         }
         $scope.parsed = [];
+        $scope.selectedColumns = [];
+        $scope.columnHoveredOn = -1;
         
         var lines = $scope.rOutput.trim().split('\n');
         
+        var maxNumColumns = -1;
         angular.forEach(lines, function (e, i) {
             var trimmed = e.trim();
             if (trimmed == '') {
@@ -31,13 +37,34 @@ app.controller('MainController', ['$scope', function ($scope) {
             }
             /* Hack: merge all < with the number after it, in case of < 2e-16 */
             var tokens = trimmed.replace('< ', '<').split(/\s+/);
+            if (tokens.length > maxNumColumns) {
+                maxNumColumns = tokens.length;
+            }
+            
             $scope.parsed.push(tokens);
         });
         
-        console.log($scope.parsed);
+        
+        for (var i = 0; i < maxNumColumns; i++) {
+            $scope.selectedColumns.push(false);
+        }
     };
     
     $scope.columnHovered = function (idx) {
         $scope.columnHoveredOn = idx;
+    };
+    
+    $scope.columnClicked = function (idx) {
+        $scope.selectedColumns[idx] = !$scope.selectedColumns[idx];
+    };
+    
+    $scope.getColumnClass = function (idx) {
+        if ($scope.selectedColumns[idx]) {
+            return 'bg-primary';
+        }
+        if ($scope.columnHoveredOn == idx) {
+            return 'bg-info';
+        }
+        return '';
     };
 }]);
